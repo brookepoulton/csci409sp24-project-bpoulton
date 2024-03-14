16823669e128cfed094f43ea62436d68811ae778
brookepoulton <brookepoulton@icloud.com>
2024-03-14 13:20:22 -0400

    Created Routes

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,26 +1,26 @@
 from rest_framework import serializers
-from .models import Vessel, VesselSchedule, BillOfLading, Container
+from core.models import Vessel, VesselSchedule, BillOfLading, Container
 
 class VesselSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vessel
-        fields = '__all__'
+        fields = ['vessel_name']
         read_only_fields = ['id']
 
 class VesselScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = VesselSchedule
-        fields = '__all__'
+        fields = ['vessel', 'voyage_number', 'arrival_date']
         read_only_fields = ['id']
 
 class BillOfLadingSerializer(serializers.ModelSerializer):
     class Meta:
         model = BillOfLading
-        fields = '__all__'
+        fields = ['voyage', 'bol_number', 'contact_name', 'contact_number', 'contact_email', 'release_status']
         read_only_fields = ['id']
 
 class ContainerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Container
-        fields = '__all__'
+        fields = ['bol', 'container_number']
         read_only_fields = ['id']
